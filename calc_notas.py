9c08867c1f0f784bda9485141d359be323b05feb
miguel domingos <miguelgames1159@gmail.com>
2025-09-02 18:28:06 -0300

    Update calc_notas.py

--- calc_notas.py
+++ calc_notas.py
@@ -15,16 +15,23 @@ def menu():
 
 
 def cadastro_nome():
-    nome_aluno = input("qual o nome do aluno?").title().strip()
+    nome_aluno = input("qual o nome do aluno? ").title().strip()
     if not nome_aluno:
         print("O nome do aluno não pode estar vazio...")
         return
     if nome_aluno in lista_alunos:
         print("nome do aluno já está cadastrado,tente denovo...")
         return
+    if not nome_aluno.replace(" ", "").isalpha():
+        print("o nome não deve conter números...")
+        return
+
+    
+
     lista_alunos.append(nome_aluno)
     lista_notas.append([])
     print(f"Parabéns {nome_aluno} !! você foi cadastrado com sucesso !")
+    
 
 def cadastro_notas():
     if not lista_alunos:
@@ -34,17 +41,23 @@ def cadastro_notas():
     print("Alunos cadastrados")
     for i in range(len(lista_alunos)):
         print(f"\n {i+1} = {lista_alunos[i]}")
+    try:
+        escolha = int(input("Escolha o número do aluno para cadastrar a nota: ")) -1
     
-    escolha = int(input("Escolha o número do aluno para cadastrar a nota: ")) -1
-    if escolha < 0 or escolha >= len(lista_alunos):
-        print("opção inválida!")
+        if escolha >= 0 and escolha < len(lista_alunos):
+            add_nota = float(input(f"Digite a nota de {lista_alunos[escolha]}: "))
+
+        else:
+            print("Erro tente novamente...")
+            return
+        
+    except ValueError:
+        print("opção inválida, digite apenas números")
         return
-    add_nota = float(input(f"Digite a nota de {lista_alunos[escolha]}: "))
+
     if add_nota < 0 or add_nota >10:
-        print("escolha uma nota de 0 a 10!!")
-        return
-
-
+                print("A nota só pode ser de 0 a 10...")
+                return
     lista_notas[escolha].append(add_nota)
     print(f" Nota {add_nota} cadastrada para {lista_alunos[escolha]}!")
 
@@ -55,22 +68,33 @@ def situacao_aluno():
     print("Alunos cadastrados:")
     for i in range(len(lista_alunos)):
         print(f"{i+1} - {lista_alunos[i]}")
-    escolha = int(input("Escolha o número do aluno para ver a situação da nota:")) -1
-    if escolha < 0 or escolha >= len(lista_alunos):
-        print("opção inválida!")
+    try:
+        
+        escolha = int(input("Escolha o número do aluno para ver a situação da nota: ")) -1
+        if not lista_notas[escolha]:
+            print("O aluno ainda não tem uma nota cadastrada")
+            return
+        
+        if escolha >= 0 and escolha < len(lista_alunos):
+            media = sum(lista_notas[escolha]) / len(lista_notas[escolha])
+            print(f"A situação do(a) {lista_alunos[escolha]} é:")
+            print(f"Notas:{lista_notas[escolha]} \n média:{media: .2f}")
+            if media > 6 or media == 6:
+                print("Situação: Aprovado! Parabéns! ")
+            else:
+                print("A média esta abaixo de 6! situação: Reprovado!")
+        else:
+            print("Erro tente novamente...")
+
+            
+    except ValueError:
+        print("opção inválida, digite apenas números.")
         return
-    if not lista_notas[escolha]:
-        print("O aluno ainda não tem uma nota cadastrada")
-        return
-    média = sum(lista_notas[escolha]) / len(lista_notas[escolha])
-    print(f"A situação do(a) {lista_alunos[escolha]} é:")
-    print(f"Notas:{lista_notas[escolha]} \n média:{média: .2f}")
-    if média >= 6:
-        print("Situação: Aprovado! Parabéns! ")
-    else:
-        print("A média esta abaixo de 6! situação: Reprovado!")
+    
+    
 
 def media_faltante():
+    media_min = 6
     if not lista_alunos:
         print("Ainda não existe nenhum aluno cadastrado.")
         return
@@ -78,36 +102,38 @@ def media_faltante():
     print("Alunos cadastrados:")
     for i in range(len(lista_alunos)):
         print(f"{i+1} - {lista_alunos[i]}")
+    try:
+        escolha = int(input("Escolha o número do aluno para calcular a média faltante: ")) - 1
+        if not lista_notas[escolha]:
+            print(f"O aluno {lista_alunos[escolha]} ainda não tem notas cadastradas.")
+            return
+        if escolha >= 0 or escolha < len(lista_alunos):
+            media_atual = sum(lista_notas[escolha]) / len(lista_notas[escolha])
+            faltante = media_min * (len(lista_notas[escolha]) + 1) - sum(lista_notas[escolha])
 
-    escolha = int(input("Escolha o número do aluno para calcular a média faltante: ")) - 1
-    if escolha < 0 or escolha >= len(lista_alunos):
-        print("Opção inválida!")
-        return
+            print(f"\nAluno: {lista_alunos[escolha]}")
+            print(f"Notas atuais: {lista_notas[escolha]}")
+            print(f"Média atual: {media_atual:.2f}")
 
-    if not lista_notas[escolha]:
-        print(f"O aluno {lista_alunos[escolha]} ainda não tem notas cadastradas.")
-        return
-    média_min = 6  
-    média_atual = sum(lista_notas[escolha]) / len(lista_notas[escolha])
-    faltante = média_min * (len(lista_notas[escolha]) + 1) - sum(lista_notas[escolha])
+            if media_atual >= 6:
+                print("✅ O aluno já atingiu a média mínima!")
+            elif faltante > 10:
+                print("⚠️ Não é possível atingir a média mínima com a próxima nota.")
+            else:
+                print(f"Para atingir média {media_min}, o aluno precisa tirar {faltante:.2f} na próxima nota.")
+        
+    except ValueError:
+        print("opção inválida, digite apenas números.")
 
-    print(f"\nAluno: {lista_alunos[escolha]}")
-    print(f"Notas atuais: {lista_notas[escolha]}")
-    print(f"Média atual: {média_atual:.2f}")
-
-    if média_atual >= 6:
-        print("✅ O aluno já atingiu a média mínima!")
-    elif faltante > 10:
-        print("⚠️ Não é possível atingir a média mínima com a próxima nota.")
-    else:
-        print(f"Para atingir média {média_min}, o aluno precisa tirar {faltante:.2f} na próxima nota.")
+    
+    
     
 
 def main():
     while True:
         menu()
-        opção = input("Que função que usar?").lower().strip()
-        match opção:
+        opcao = input("\nQue função que usar? ").lower().strip()
+        match opcao:
             case "1":
                 cadastro_nome()
             case "2":
